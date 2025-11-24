9381f6706ea9e9d711fbb22166cce258e27c3d06
zakyalfrss <blackgamerr08@gmail.com>
2025-11-24 11:38:01 +0700

    saya sudah belajar array dan pengulangan

--- first.go
+++ first.go
@@ -4,13 +4,14 @@ import "fmt"
 
 
 func main()  {
-	var Nums[5] int
+	var Nums [5]int
 		Nums[0] = 0
 		Nums[1] = 45
 		Nums[2] = 65
 		Nums[3] = 80
 		Nums[4] = 90
-	for i := 0; i < len(nums); i++ {
-		fmt.Println(Nums[i])
+
+	for  _,i := range Nums {
+		fmt.Println(i)
 	}
 }
